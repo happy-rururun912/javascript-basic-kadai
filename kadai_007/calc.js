fadf2d81207858db45413dd63beeb86fadb8950f
happy-rururun912 <hrfhrk1212@gmail.com>
2024-12-08 23:02:36 +0900

    課題007 js編集

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,20 +1,21 @@
 //１以上の値を代入
-let num = 10;
+let num = 15;
+
+//3と５の倍数なら
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
 //３の倍数なら
-if (num === 3) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 //５の倍数なら
-else if (num === 5) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
-}
-//3と５の倍数なら
-else if (num === 3,5) {
-  console.log('3と5の倍数です');
 }
 //それ以外の時は
 else {
-  console.log('num');
+  console.log(num);
 }
 
 
