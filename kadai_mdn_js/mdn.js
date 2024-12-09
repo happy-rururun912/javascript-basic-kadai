8869cb235cd87f57194b51d950fe13f71828add4
happy-rururun912 <hrfhrk1212@gmail.com>
2024-12-09 13:42:26 +0900

    課題mdn js 修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const today = new Date();
 //今日の年月日
 const year = today.getFullYear();
-const month = today.getMonth();
-const date = today.getDate();
-console.log(today);
+const month = today.getMonth() +1 ;
+const day = today.getDate();
+console.log(year + '年' + month + '月' + day + '日');
