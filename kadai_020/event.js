9db1ac72b54692ecf2eafa4c7d88d071c5a525d5
happy-rururun912 <hrfhrk1212@gmail.com>
2024-12-10 09:36:30 +0900

    課題020

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+// btnとtextを取得し、定数に代入する
+const btn = document.getElementById('btn','text');
+ 
+// クリックされたときイベント処理
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+  console.log('ボタンをクリックしました');
+});
